d484e352ea67657ce6b327a9926b26dce2dae9e4
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-02-01 22:39:42 +0700

    add js callback

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -20,9 +20,24 @@
       payButton.addEventListener('click', function () {
 
         // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token
-        window.snap.pay('{{ $snap_token }}');
-
-        // customer will be redirected after completing payment pop-up
+        window.snap.pay('{{$snap_token}}', {
+            onSuccess: function(result){
+            /* You may add your own implementation here */
+            alert("payment success!"); console.log(result);
+          },
+          onPending: function(result){
+            /* You may add your own implementation here */
+            alert("wating your payment!"); console.log(result);
+          },
+          onError: function(result){
+            /* You may add your own implementation here */
+            alert("payment failed!"); console.log(result);
+          },
+          onClose: function(){
+            /* You may add your own implementation here */
+            alert('you closed the popup without finishing the payment');
+          }
+        })
       });
     </script>
   </body>
