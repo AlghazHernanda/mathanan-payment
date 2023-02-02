2570da423fd721d29e6afc41e711874d08e76682
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-02-02 20:10:26 +0700

    add json decode

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function payment_handler(Request $request)
+    {
+        // //untuk coba-coba ambil data di body postman
+        // return $request->getContent();
+
+        //ubah data ke bentuk JSON
+        $json = json_decode($request->getContent());
+    }
 }
