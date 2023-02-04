5997e48635b83e7bd7a1c3c9321f1f5918af1dbd
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-02-04 17:48:52 +0700

    add auth validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -9,5 +10,25 @@ class AuthController extends Controller
     public function get_login()
     {
         return view('login');
+    }
+
+    public function get_register()
+    {
+        return view('register');
+    }
+
+    public function post_login(Request $request)
+    {
+    }
+
+    public function post_register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::create($request->except(['_token']));
     }
 }
