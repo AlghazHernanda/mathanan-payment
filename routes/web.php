8277f9c9bcbb7d9ed4ce2d2549b6662c1b5f683e
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-02-05 11:46:19 +0700

    add route for verification and  middleware

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,23 @@ Route::get('/', [WebController::class, 'index']);
 Route::get('/payment', [WebController::class, 'payment']);
 Route::post('/payment', [WebController::class, 'payment_post']);
 
-//verifikasi
+//bagian verifikasi
 Route::get('/login', [AuthController::class, 'get_login'])->name('login');
 Route::get('/register', [AuthController::class, 'get_register'])->name('register');
 
 Route::post('/login', [AuthController::class, 'post_login']);
 Route::post('/register', [AuthController::class, 'post_register']);
 
+//verifikasi email
 Route::get('/email//verify/need-verification', [VerificationController::class, 'notice'])->middleware('auth')->name('verification.notice');
 Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verify'])->middleware('auth', 'signed')->name('verification.verify');
+
+//kirim link verifikasi lagi
+Route::get('/email//verify/resend-verification', [VerificationController::class, 'send'])->middleware('auth', 'throttle:6,1')->name('verification.send');
+
+//ini akan bisa diakses setelah user sudah verifikasi email, atau di database nya email_verified_at nya sudah ke isi
+Route::middleware(['auth', 'auth.session', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return "selamat datang didashboard";
+    });
+});
