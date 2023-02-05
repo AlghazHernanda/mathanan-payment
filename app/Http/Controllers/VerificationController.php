c96fda2aa5968867bbc934f315f3c81c824ab264
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-02-05 11:41:50 +0700

    add logic verifikasi

---
+++ app/Http/Controllers/VerificationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+
+class VerificationController extends Controller
+{
+    public function notice()
+    {
+        //ini harusnya bisa dijadikan view biar enak
+        return "mohon untuk melakukan verifikasi email";
+    }
+
+    public function verify(EmailVerificationRequest $request)
+    {
+        $request->fulfill();
+        return "berhasil diverifikasi, selamat datang di dahsboard";
+    }
+}
